f0db7125366580ddfd57041e158303543bd4fabc
Jarett Sisk <jarett.sisk@gmail.com>
2024-01-25 18:32:53 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {useEffect, useState} from 'react'
+
+function App() {
+    const [count, setCount] = useState(0)
+    const [message, setMessage] = useState(null);
+
+    const handleIncrement = () => {
+      setCount(count + 1);
+    }
+    useEffect(() => {
+        // runs on initial render, and then again on count changes.
+        setMessage(`Youve clicked increment ${count} times!`)
+    }, [count])
+  return (
+    <>
+        <div style={{display: "flex", alignItems: "center", flexDirection: "column", width: "100vw"}}>
+            <h1>{message}</h1>
+            <button onClick={handleIncrement}>INCREMENT</button>
+        </div>
+
+    </>
+  )
+}
+
+export default App
